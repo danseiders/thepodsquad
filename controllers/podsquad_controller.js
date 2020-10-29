5d634c6aa30d2f1791c1ffb421e18131ff384bc4
Dan Seiders <dseiders333@gmail.com>
2020-10-29 13:28:35 -0400

    create route up and working, general file structure built

---
+++ controllers/podsquad_controller.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const router = express.Router()
+const Pod = require('../models/pods.js')
+
+
+//index 
+// router.get('/', (req, res) => {
+//     Pog.find({}, (error, allPods) => {
+//         res.render('index.ejs', {
+//             pods: allPods
+//         })
+//     })       
+// })
+
+//new
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+//create
+router.post('/', (req, res) => {
+    console.log(req.params)
+    Pod.create(req.body, (error, createdPod) => {
+        res.redirect('/pods')
+    })
+})
+
+// //seed
+// router.get('/seed', (req, res) => {
+//     Pod.create([
+//         {
+//             title: 'another day',
+//             entry: 'al;dsjf adslkfjadfs al;fjkadsl;fads asdf;ljkdasf;jlkf adsfl;jkadfs; asdfl;jkadfs;ljfadsj asdfljdam,.n,zl;akjadjsf0io',
+//             shipIsBroken: false
+//         },
+//         {
+//             title: 'broken ship',
+//             entry: 'aasdfj9040034ht8h2gjnwasjlkasdjk vvlkdsfab asdklhjafds adfklj io',
+//             shipIsBroken: true
+//         },
+//         {
+//             title: 'the end',
+//             entry: 'nxcvn,.cvxzn,.cvxjkl;adsjkldafsjl09w098 7032478243hiadfsbklmncvbm,cvxzb',
+//             shipIsBroken: false
+//         }
+//     ])
+// })
+
+
+// show
+// router.get('/:id', (req, res) => {
+//     Pod.findById(req.params.id, (err, foundPod) => {
+//         res.render('show.ejs', {
+//             pod: foundPod
+//         })
+//     })
+// })  
+
+//delete
+// router.delete('/:id', (req, res) => {
+//     Pod.findByIdAndDelete(req.params.id, () => {
+//         res.redirect('/pods')
+//     })
+// })
+
+//edit
+// router.get('/:id/edit', (req, res) => {
+//     Pod.findById(req.params.id, (err, foundPod) => {
+//         res.render('edit.ejs', {
+//             pod: foundPod
+//         })
+//     })
+// })
+
+//put
+// router.put('/:id', (req, res) => {
+//     console.log(req.params.id)
+//     console.log(req.body)
+//     Pod.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedPod) => {
+//         if(err){
+//             console.log(err)
+//         }
+//         res.redirect('/pods')
+//     })
+// })
+
+
+module.exports = router
