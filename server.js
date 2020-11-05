0b5f017d7dc19cdb93f18b17a48f83816bf98e72
Dan Seiders <dseiders333@gmail.com>
2020-11-05 12:06:54 -0500

    test heroku

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ const methodOverride  = require('method-override');
 const PORT = process.env.PORT
 //DATABASE
 const MONGODB_URI = process.env.MONGODB_URI 
+
+
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true} )
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'))
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+db.on('open' , ()=>{})
+
+//MIDDLEWARE
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(methodOverride('_method'))
 app.use(
     session({
         secret: process.env.SECRET,
@@ -17,18 +30,6 @@ app.use(
         saveUninitialized: false 
     })
 )
-
-mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false} );
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
-db.on('disconnected', () => console.log('mongo disconnected'));
-db.on('open' , ()=>{});
-
-//MIDDLEWARE
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(methodOverride('_method'));
 
 const sessionsController = require('./controllers/sessions_controller.js')
 app.use('/sessions', sessionsController)
